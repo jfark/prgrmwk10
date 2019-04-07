8e6b1135eeb645d46f7c3b8638c4b031cfce35ea
jfark <jessamyn.farkas@gmail.com>
2019-04-07 17:59:47 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var signedBookCollection: [
+	{
+		author: "Oates, Joyce Carol",
+		title: "The Lost Landscape: A Writer's Coming of Age",
+		year: 2015,
+		didIEmbarassMyself: "true",
+		haveIReadBookYet: "false",
+		inscription: "For Jessa -Joyce Carol Oates",
+		writingGroupFriend: "false"
+	},
+	{
+		author: "Yanagihara, Hanya",
+		title: "A Little Life",
+		year: 2015,
+		didIEmbarassMyself: "false",
+		haveIReadBookYet: "true",
+		inscription: "in March 2016: To Jessa - with thanks for reading and keep writing - Hanya Yanagihara",
+		writingGroupFriend: "false"
+	},
+	{
+		author: "Gaitskill, Mary",
+		title: "Bad Behavior",
+		year: 1988,
+		didIEmbarassMyself: "true",
+		haveIReadBookYet: "true",
+		inscription: "For Jessa -Mary Gaitskill",
+		writingGroupFriend: "false"
+	},
+	{
+		author: "Clemmons, Zinzi",
+		title: "What We Lose",
+		year: 2017,
+		didIEmbarassMyself: "false",
+		haveIReadBookYet: "true",
+		inscription: "Jessa, Long time! Enjoy! 7-13-17 Zinzi Clemmons",
+		writingGroupFriend: "true"
+	},
+	{
+		author: "Sharma, Akhil",
+		title: "A Life of Adventure and Delight",
+		year: 2017,
+		didIEmbarassMyself: "true",
+		haveIReadBookYet: "false",
+		inscription: "Dear Jessa, All good wishes. I hope you and your work go well. Akhil Sharma",
+		writingGroupFriend: "false"
+	}
+]
