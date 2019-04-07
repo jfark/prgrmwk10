ae6fc0fc16b793b74a36c444514eeed6ea5f6dcd
jfark <jessamyn.farkas@gmail.com>
2019-04-07 19:46:37 -0400

    Conditional Implemented

--- script.js
+++ script.js
@@ -2,37 +2,37 @@ const bookArray = [
 	{
 		author: "Oates, Joyce Carol",
 		title: "The Lost Landscape: A Writer's Coming of Age",
-		didIEmbarassMyself: "true",
+		didIEmbarassMyself: true,
 		inscription: '"For Jessa -Joyce Carol Oates"',
-		writingGroupFriend: "false"
+		writingGroupFriend: false
 	},
 	{
 		author: "Yanagihara, Hanya",
 		title: "A Little Life",
-		didIEmbarassMyself: "false",
+		didIEmbarassMyself: false,
 		inscription: '"in March 2016: To Jessa - with thanks for reading and keep writing - Hanya Yanagihara"',
-		writingGroupFriend: "false"
+		writingGroupFriend: false
 	},
 	{
 		author: "Gaitskill, Mary",
 		title: "Bad Behavior",
-		didIEmbarassMyself: "true",
+		didIEmbarassMyself: true,
 		inscription: '"For Jessa -Mary Gaitskill"',
-		writingGroupFriend: "false"
+		writingGroupFriend: false
 	},
 	{
 		author: "Clemmons, Zinzi",
 		title: "What We Lose",
-		didIEmbarassMyself: "false",
+		didIEmbarassMyself: false,
 		inscription: '"Jessa, Long time! Enjoy! 7-13-17 Zinzi Clemmons"',
-		writingGroupFriend: "true"
+		writingGroupFriend: true
 	},
 	{
 		author: "Sharma, Akhil",
 		title: "A Life of Adventure and Delight",
-		didIEmbarassMyself: "true",
+		didIEmbarassMyself: true,
 		inscription: '"Dear Jessa, All good wishes. I hope you and your work go well. Akhil Sharma"',
-		writingGroupFriend: "false"
+		writingGroupFriend: false
 	}
 ];
 
